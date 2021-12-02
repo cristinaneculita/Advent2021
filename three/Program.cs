f1ffb9552aff1c7c75ff419de2f0fb5e1f9eb771
Neculita Cristina <cristina.neculita@scania.com>
2021-12-02 19:28:57 +0200

    Initial

---
+++ three/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace advent
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] lines = System.IO.File.ReadAllLines("input.txt");
+
+            // Display the file contents by using a foreach loop.
+            //System.Console.WriteLine("Contents of input.txt = ");
+            var response=0;
+            foreach (string line in lines)
+            {
+                // Use a tab to indent each line of the file.
+               // Console.WriteLine("\t" + line);
+            }
+            
+            Console.WriteLine("raspunsul este: "+response);
+            //Console.WriteLine("Hello World!");
+        }
+    }
+}
