0bb7a88d6faa6ee35cdbde551d8c706c2b7789c9
Neculita Cristina <cristina.neculita@scania.com>
2021-12-07 09:33:20 +0200

    initial

--- three/Program.cs
+++ three/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace advent
 {
@@ -7,6 +9,7 @@ namespace advent
         static void Main(string[] args)
         {
             string[] lines = System.IO.File.ReadAllLines("input.txt");
+            var coord = lines[0].Split(",").ToList().Select(e => Int32.Parse(e)).ToList();
 
             // Display the file contents by using a foreach loop.
             //System.Console.WriteLine("Contents of input.txt = ");
