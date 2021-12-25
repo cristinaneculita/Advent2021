4a5e878229f64507a8499f89f1c795a573dc8477
Neculita Cristina <cristina.neculita@scania.com>
2021-12-25 19:11:25 +0200

    shit

---
+++ ConsoleApp4/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+using System.Linq;
+using System.Diagnostics;
+
+namespace AdventOfCode2021
+{
+    class Day24
+    {
+        static void Main(string[] args)
+        {
+            PartOne();
+        }
+            private static readonly String[] lines = File.ReadAllLines(@"Input.txt");
+
+        
+        public static (long result, long time) PartOne()
+        {
+            var sw = new Stopwatch();
+            sw.Start();
+            var result = long.Parse(string.Join("", RunData(1).OrderBy(x => x.Key).Select(x => x.Value)));
+            sw.Stop();
+
+            return (result, sw.ElapsedMilliseconds);
+        }
+
+        public static (long result, long time) PartTwo()
+        {
+            var sw = new Stopwatch();
+            sw.Start();
+            var result = long.Parse(String.Join("", RunData(2).OrderBy(x => x.Key).Select(x => x.Value)));
+            sw.Stop();
+
+            return (result, sw.ElapsedMilliseconds);
+        }
+
+        private static Dictionary<int, int> RunData(int part)
+        {
+            var pairs = new List<(int, int)>();
+            foreach (var i in Enumerable.Range(0, 14))
+            {
+                pairs.Add((int.Parse(lines[i * 18 + 5][6..]), int.Parse(lines[i * 18 + 15][6..])));
+            }
+            var stack = new Stack<(int, int)>();
+            var keys = new Dictionary<int, (int x, int y)>();
+
+            foreach (var (pair, i) in pairs.Select((pair, i) => (pair, i)))
+            {
+                if (pair.Item1 > 0)
+                {
+                    stack.Push((i, pair.Item2));
+                }
+                else
+                {
+                    var (j, jj) = stack.Pop();
+                    keys[i] = (j, jj + pair.Item1);
+                }
+            }
+            var output = new Dictionary<int, int>();
+
+            foreach (var (key, val) in keys)
+            {
+                output[key] = part == 1 ? Math.Min(9, 9 + val.y) : Math.Max(1, 1 + val.y);
+                output[val.x] = part == 1 ? Math.Min(9, 9 - val.y) : Math.Max(1, 1 - val.y);
+            }
+
+            return output;
+        }
+    }
+}
--- three/Program.cs
+++ three/Program.cs
@@ -4,25 +4,320 @@ using System.Linq;
 
 namespace advent
 {
+
+
+
     class Program
     {
-       
+        public class Operation
+        {
+            public string Type { get; set; }
+            public string Term1 { get; set; }
+            public string Term2 { get; set; }
+            public Operation(string type, string term1, string term2)
+            {
+                Type = type;
+                Term1 = term1;
+                Term2 = term2;
+            }
+
+            public bool Apply()
+            {
+                switch (Type)
+                {
+                    case "inp":
+                        w = numberchar[numberindex] - '0';
+                        numberindex++;
+                        break;
+                    case "add":
+                        Sum(Term1, Term2);
+                        break;
+                    case "mul":
+                        Mul(Term1, Term2);
+                        break;
+                    case "div":
+                        return Div(Term1, Term2);
+                    case "mod":
+                        return Mod(Term1, Term2);
+                    case "eql":
+                        Eql(Term1, Term2);
+                        break;
+                }
+
+                return true;
+            }
+
+            private void Eql(string term1, string term2)
+            {
+                double t1, t2;
+                t1 = GetTerm1(term1);
+                t2 = GetTerm2(term2);
+                t1 = (t1 == t2) ? 1 : 0;
+                switch (term1)
+                {
+                    case "x": x = t1; break;
+                    case "y": y = t1; break;
+                    case "z": z = t1; break;
+                    case "w": w = t1; break;
+                }
+            }
+
+            private bool Mod(string term1, string term2)
+            {
+                double t1, t2;
+                t1 = GetTerm1(term1);
+                if (t1 < 0)
+                    return false;
+                t2 = GetTerm2(term2);
+                if (t2 <= 0)
+                    return false;
+                t1 = t1 % t2;
+                switch (term1)
+                {
+                    case "x": x = t1; break;
+                    case "y": y = t1; break;
+                    case "z": z = t1; break;
+                    case "w": w = t1; break;
+                }
+                return true;
+            }
+
+            private bool Div(string term1, string term2)
+            {
+                double t1, t2;
+                t1 = GetTerm1(term1);
+
+                t2 = GetTerm2(term2);
+                if (t2 == 0)
+                    return false;
+                t1 = Math.Truncate(t1 / t2);
+                switch (term1)
+                {
+                    case "x": x = t1; break;
+                    case "y": y = t1; break;
+                    case "z": z = t1; break;
+                    case "w": w = t1; break;
+                }
+                return true;
+            }
+
+            private void Sum(string term1, string term2)
+            {
+                double t1, t2;
+                t1 = GetTerm1(term1);
+                t2 = GetTerm2(term2);
+                t1 = t1 + t2;
+                switch (term1)
+                {
+                    case "x": x = t1; break;
+                    case "y": y = t1; break;
+                    case "z": z = t1; break;
+                    case "w": w = t1; break;
+                }
+            }
+
+            private void Mul(string term1, string term2)
+            {
+                double t1, t2;
+                t1 = GetTerm1(term1);
+                t2 = GetTerm2(term2);
+                t1 = t1 * t2;
+                switch (term1)
+                {
+                    case "x": x = t1; break;
+                    case "y": y = t1; break;
+                    case "z": z = t1; break;
+                    case "w": w = t1; break;
+                }
+            }
+
+            private double GetTerm2(string term2)
+            {
+                switch (term2)
+                {
+                    case "x": return x;
+                    case "y": return y;
+                    case "z": return z;
+                    case "w": return w;
+                    default:
+                        return double.Parse(term2);
+                }
+            }
+
+            private double GetTerm1(string term1)
+            {
+                switch (term1)
+                {
+                    case "x": return x;
+                    case "y": return y;
+                    case "z": return z;
+                    case "w": return w;
+                    default:
+                        break;
+                }
+                return 0;
+            }
+        }
+
+        public static double x;
+        public static double y;
+        public static double z;
+        public static double w;
+        public static char[] numberchar = new char[14];
+        public static int numberindex;
+        public static int[] terminations;
+        public static int[] hundreds;
+        public static double[] hund;
+        public static int hundindex;
+        public static bool oneatatime;
         static void Main(string[] args)
         {
             string[] lines = System.IO.File.ReadAllLines("input.txt");
-
+            List<Operation> operations = new List<Operation>();
 
             //var coord = lines[0].Split(",").ToList().Select(e => Int32.Parse(e)).ToList();
 
             // Display the file contents by using a foreach loop.
             //System.Console.WriteLine("Contents of input.txt = ");
             var res = 0;
+            oneatatime = false;
             foreach (var line in lines)
             {
-               
+                var x = line.Split(' ');
+                var thirdp = x.Count() == 2 ? "" : x[2];
+
+                var op = new Operation(x[0], x[1], thirdp);
+                operations.Add(op);
             }
-            Console.WriteLine("response: " + res);
+            //double number = 69914999975370;
+            double number = 99999999979512;
+            
+            terminations = new int[7] { 79, 68,57,46,35,24,13};
+            hundreds = new int[7] { 1, 2, 3, 4, 5, 6, 7 };
+            hund = new double[4] {11000, 1100, 110, 11 };
+            hundindex = 1;
+            bool resulttotal = false;
+            do
+            {
+                z = 15376;
+                number = nextNumber(number, z);
+                numberchar = number.ToString().ToCharArray();
+                numberindex = 0;
+                if (numberchar.Contains('0'))
+                    continue;
+                x = 0;
+                y = 0;
+                z = 0;
+                w = 0;
+                
+                resulttotal = false;
+                foreach (var oper in operations)
+                {
+                    var result = oper.Apply();
+                    resulttotal |= result;
+                    if (!result)
+                        break;
+                }
+                if (z == 0)
+                {
+                    resulttotal = true;
+                }
+                else { resulttotal = false;
+                    if (z <=15376)
+                    {
+                        Console.Write(" " + number);
+                        Console.WriteLine(" z:" + z);
+                    }
+                }
+
+                number = nextNumber(number, z);
+
+              
+
+
+            } while (!resulttotal);
+
+
+
+            Console.WriteLine("response: " + number);
 
         }
+
+        private static double nextNumber(double number, double z)
+        {
+            if (z == 15376)
+            {
+               
+                return number - hund[hundindex];
+            }
+            else if (!oneatatime)
+            {
+                var anterior = number + hund[hundindex];
+                hundindex++;
+                oneatatime = true;
+                return anterior+1;
+            }
+            else {
+                return number + 1;
+
+            }
+
+            //int s = (int)(numberchar[12]);
+            //int z = (int)number % 100;
+            //if (hundreds.Contains(s))
+            //{
+            //    switch (s)
+            //    {
+            //        case 7: if (z > 91)
+            //                return number - 1;
+            //            else if (z == 91)
+            //                return number-12;
+            //            else if (z != 13)
+            //                return number - 11;
+            //            else return number - 24;
+            //        case 6:
+            //            if (z > 81)
+            //                return number - 1;
+            //            else if (z == 81)
+            //                return number - 2;
+            //            else if (z != 13)
+            //                return number - 11;
+            //            else return number - 14;
+            //        case 5:
+            //            if (z > 11)
+            //                return number - 1;
+
+            //            else return number - 32;
+            //        case 4:
+            //            if (z == 79)
+            //                return number - 10;
+            //            else if (z > 61)
+            //                return number - 1;
+            //            else if (z == 61)
+            //                return number - 4;
+            //            else if (z > 13)
+            //                return number - 11;
+            //            else 
+            //                return number - 34;
+            //        case 3:
+            //            if (z == 79)
+            //                return number - 11;
+            //            else if (z == 68)
+            //                return number - 9;
+            //            else if (z >51)
+            //                return number - 1;
+            //            else if (z ==51)
+            //                return number - 11;
+            //            else
+            //                return number - 34;
+
+
+            //    }
+            return number - 1;
+            }
+
+        
+
+        
     }
 }
